f68710633f242c4481fd23b1c954686e555ffef7
fredboys <freddieboys@hotmail.co.uk>
2022-11-25 18:49:14 +0000

    Mocking

--- student.py
+++ student.py
@@ -1,7 +1,9 @@
 from datetime import date, timedelta
+import requests
+
 
 class Student:
-    """A student class as base for method testing"""
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -10,16 +12,25 @@ class Student:
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
-
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
-
 
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
-
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
